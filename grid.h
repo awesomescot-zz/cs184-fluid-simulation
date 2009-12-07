8d493c7fd1ae91a8870300421fdea20920d79f7a
scot.spinner <scot.spinner@82907ca0-e064-11de-ab17-5d3be3536607>
2009-12-07 18:13:45 +0000

    new grid.

--- grid.h
+++ grid.h
@@ -1,15 +1,26 @@
+#include <iostream>
+#include "algebra3.h"
+
 #ifndef _grid_h
 #define _grid_h
 
 using namespace std;
 
+struct cube{
+	float u, v, w, temp, comp;
+};
+
 class grid {
 public:
-	vec3 location;
+	cube ***cubeGrid;
+	float x, y, z;
+	int xSplit, ySplit, zSplit;
+	float xCubeSize, yCubeSize, zCubeSize;
 	grid(){};
-	grid(vec3);
-	ray getRay(vec3);
-	void setCord(vec3);
+	grid(vec3, vec3, vec3, vec3, int, int, int);
+	cube getCube(int, int, int);
+	cube getCube(vec3);
+	vec3 getVelosity(vec3);
 };
 
 #endif
